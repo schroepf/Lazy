8cc311ddaa9f212fa1d738f611ae0f0bcb5b2d62
Tobias Schröpf <tobias.schroepf@jambit.com>
2018-11-06 20:00:10 +0100

    Initial Commit

---
+++ Lazy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lazy
+//
+//  Created by Tobias Schröpf on 06.11.18.
+//  Copyright © 2018 Tobias Schröpf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
