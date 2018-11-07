826cdb75b21a7e1469df71480ef3cf85ca318f3f
Tobias Schröpf <tobias.schroepf@jambit.com>
2018-11-07 01:43:35 +0100

    Lazy loading

--- Lazy/ViewController.swift
+++ Lazy/ViewController.swift
@@ -10,53 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    enum LoadingError: Error {
+        case internalError(String)
+    }
+    
     fileprivate static let reuseIdentifier = "DefaultCell"
     
     @IBOutlet weak var collectionView: UICollectionView!
     
-    fileprivate let data: [UIColor?]
+    fileprivate let cache:ListItemCache<UIColor>
     
     required init?(coder aDecoder: NSCoder) {
-        data = [
-            UIColor.random(),
-            UIColor.random(),
-            UIColor.random(),
-            UIColor.random(),
-            UIColor.random(),
-            UIColor.random(),
-            UIColor.random(),
-            UIColor.random(),
-            UIColor.random(),
-            UIColor.random(),
-            UIColor.random(),
-            UIColor.random(),
-            UIColor.random(),
-            UIColor.random(),
-            UIColor.random(),
-            UIColor.random(),
-            UIColor.random(),
-            UIColor.random(),
-            UIColor.random(),
-            UIColor.random(),
-            UIColor.random(),
-            UIColor.random(),
-            UIColor.random(),
-            UIColor.random(),
-            UIColor.random(),
-            UIColor.random(),
-            UIColor.random(),
-            UIColor.random(),
-            UIColor.random(),
-            nil,
-            nil,
-            nil,
-            nil,
-            nil,
-            nil,
-            nil,
-            nil
-        ]
+        cache = ListItemCache<UIColor>(cacheMissHandler: { (index, onSuccess, onError)  in
+            print("fetching item at index: \(index)")
+            
+            if index == 20 {
+                onSuccess(nil)
+                return
+            }
+            
+            if index % 5 == 0 {
+                onError(LoadingError.internalError("ERROR"))
+            }
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0, execute: {
+                onSuccess(UIColor.random())
+            })
+        })
+        
         super.init(coder: aDecoder)
+        
+        cache.delegate = self
     }
     
     override func viewDidLoad() {
@@ -71,14 +55,14 @@ class ViewController: UIViewController {
 
 extension ViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return data.count
+        return cache.size()
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ViewController.reuseIdentifier,
                                                       for: indexPath) as! DefaultCell
         
-        cell.bind(to: data[indexPath.row])
+        cell.bind(to: cache[indexPath.row])
         
         return cell
     }
@@ -86,4 +70,12 @@ extension ViewController: UICollectionViewDataSource {
 
 extension ViewController: UICollectionViewDelegate {
     
+}
+
+extension ViewController: ListItemCacheDelegate {
+    func didUpdate() {
+        DispatchQueue.main.async { [weak self] in
+            self?.collectionView.reloadData()
+        }
+    }
 }
