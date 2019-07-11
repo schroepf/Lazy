2982faddf0c9430d7531fa049b259eb370ee9fef
Tobias Schröpf <tobias.schroepf@jambit.com>
2019-07-11 23:28:32 +0200

    Reformat code

--- Lazy/LazyList.swift
+++ Lazy/LazyList.swift
@@ -15,7 +15,7 @@ import Foundation
 struct LazyResult<Element> {
     let value: Element?
     let error: Error?
-    
+
     func isEmpty() -> Bool {
         return value == nil && error == nil
     }
@@ -30,108 +30,105 @@ extension LazyResult: CustomDebugStringConvertible {
 typealias Index = Int
 
 // pass 'nil' as argument to this callback for items which don't exist (i.e. "out of bounds") - to signal that there is no item ...
-typealias SuccessCallback<Result> = (Result?) -> ()
-typealias ErrorCallback = (Error) -> ()
-typealias LoadItemHandler<Result> = (Index, @escaping SuccessCallback<Result>, @escaping ErrorCallback) -> ()
+typealias SuccessCallback<Result> = (Result?) -> Void
+typealias ErrorCallback = (Error) -> Void
+typealias LoadItemHandler<Result> = (Index, @escaping SuccessCallback<Result>, @escaping ErrorCallback) -> Void
 
 private struct LazyRequest<Element> {
     // while result is "nil" the request is considered as ongoing...
     let result: LazyResult<Element>?
-    
+
     func hasResult() -> Bool {
         return result != nil
     }
-    
+
     func isLoading() -> Bool {
         return result == nil
     }
-    
+
     static func from(item: Element?) -> LazyRequest? {
         return item == nil ? nil : LazyRequest(result: LazyResult(value: item, error: nil))
     }
-    
+
     static func wrap(value: Element? = nil, error: Error? = nil) -> LazyRequest {
         return LazyRequest(result: LazyResult(value: value, error: error))
     }
 }
 
-
 class LazyList<Element> {
-    
     // A helper class to manage and synchronize access to the requests array...
     private class RequestsAccess<Element> {
         private let accessQueue = DispatchQueue(label: "LazyList.RequestsAccess")
         private let onChanged: (() -> Void)
-        
+
         init(onChanged: @escaping (() -> Void)) {
             self.onChanged = onChanged
         }
-        
+
         private var requests: [LazyRequest<Element>?] = [nil] {
             didSet {
                 onChanged()
             }
         }
-        
+
         fileprivate subscript(index: Index) -> LazyRequest<Element>? {
             get {
                 return accessQueue.sync { requests[index] }
             }
-            
+
             set {
                 accessQueue.async {
                     self.requests[index] = newValue
                 }
             }
         }
-        
+
         fileprivate func readAll() -> [LazyRequest<Element>?] {
             return accessQueue.sync { requests }
         }
-        
+
         fileprivate func update(_ block: ([LazyRequest<Element>?]) -> ([LazyRequest<Element>?])) {
             accessQueue.sync {
                 requests = block(requests)
             }
         }
-        
+
         fileprivate func updateAsync(_ block: @escaping ([LazyRequest<Element>?]) -> ([LazyRequest<Element>?])) {
             accessQueue.async {
                 self.requests = block(self.requests)
             }
         }
-        
-        fileprivate func sync(_ block: () -> ()) {
+
+        fileprivate func sync(_ block: () -> Void) {
             accessQueue.sync {
                 block()
             }
         }
-        
-        fileprivate func async(_ block: @escaping () -> ()) {
+
+        fileprivate func async(_ block: @escaping () -> Void) {
             accessQueue.async {
                 block()
             }
         }
     }
-    
+
     private let callbackQueue = DispatchQueue(label: "LazyList.Callbacks")
-    
+
     private let onLoadBefore: LoadItemHandler<[Element]>
     private let onLoadItem: LoadItemHandler<Element>
     private let onLoadAfter: LoadItemHandler<[Element]>
     private let onChanged: (() -> Void)?
-    
+
     private lazy var requests = RequestsAccess<Element>(onChanged: {
         guard let onChanged = self.onChanged else {
             return
         }
-        
+
         self.callbackQueue.async {
             onChanged()
         }
     })
-    
-    
+
     // TODO: Add documentation!
     init(onLoadBefore: @escaping LoadItemHandler<[Element]>,
          onLoadItem: @escaping LoadItemHandler<Element>,
@@ -142,56 +139,56 @@ class LazyList<Element> {
         self.onLoadAfter = onLoadAfter
         self.onChanged = onChanged
     }
-    
+
     // TODO: Add documentation
     subscript(index: Index) -> LazyResult<Element>? {
         prefetch(index: index)
         return requests[index]?.result
     }
-    
+
     // TODO: Add documentation
     func prefetch(index: Index) {
         guard requests[index] == nil else {
             // a request is already started...
             return
         }
-        
-        self.requests.update { (requests) -> ([LazyRequest<Element>?]) in
+
+        requests.update { (requests) -> ([LazyRequest<Element>?]) in
             var result = requests
-            result[index] = LazyRequest(result: nil)    // FIXME: This triggers an unnecessary onChanged() call
-            
-            if index == 0 && requests.count > 1 {
+            result[index] = LazyRequest(result: nil) // FIXME: This triggers an unnecessary onChanged() call
+
+            if index == 0, requests.count > 1 {
                 triggerLoadBefore(index: index + 1)
             } else if index == requests.count - 1 {
                 triggerLoadAfter(index: index - 1)
             } else {
                 triggerLoadItem(index: index)
             }
-            
+
             return result
         }
     }
-    
+
     // TODO: Add documentation
     // nil values in the result array are considered as "loading" items...
     func items() -> [LazyResult<Element>?] {
         let requests = self.requests.readAll()
-        
+
         guard requests.count > 0 else {
             // if there are no requests yet just emit a placeholder...
             return [nil]
         }
-        
+
         var results = requests
-            .filter{ (request) in
+            .filter { request in
                 guard let result = request?.result else {
                     return true
                 }
-                
+
                 return !result.isEmpty()
             }
             .map { $0?.result }
-        
+
         // if the last item is not an error or not empty (i.e. if it has a value add a placeholder to the bottom:
         if let _ = requests.last??.result?.value {
             results.append(nil)
@@ -199,7 +196,7 @@ class LazyList<Element> {
 
         return results
     }
-    
+
     func update(_ item: Element?, at index: Index) {
         requests.update { (requests) -> ([LazyRequest<Element>?]) in
             var result = requests
@@ -207,7 +204,7 @@ class LazyList<Element> {
             return result
         }
     }
-    
+
     func insert(_ item: Element?, at index: Index) {
         requests.update { (requests) -> ([LazyRequest<Element>?]) in
             var result = requests
@@ -215,25 +212,25 @@ class LazyList<Element> {
             return result
         }
     }
-    
+
     func insert(contentsOf items: [Element?], at index: Index) {
-        let requestsToAdd = items.map{ LazyRequest.from(item: $0) }
-        
+        let requestsToAdd = items.map { LazyRequest.from(item: $0) }
+
         requests.update { (requests) -> ([LazyRequest<Element>?]) in
             var result = requests
             result.insert(contentsOf: requestsToAdd, at: index)
             return result
         }
     }
-    
+
     func append(_ item: Element?) {
         insert(item, at: requests.readAll().count)
     }
-    
+
     func append(contentsOf items: [Element?]) {
         insert(contentsOf: items, at: requests.readAll().count)
     }
-    
+
     func remove(at index: Index) {
         requests.update { (requests) -> ([LazyRequest<Element>?]) in
             var result = requests
@@ -241,47 +238,47 @@ class LazyList<Element> {
             return result
         }
     }
-    
+
     func clear() {
         requests.update { (_) -> ([LazyRequest<Element>?]) in
-            return [nil]
+            [nil]
         }
     }
-    
+
     private func triggerLoadBefore(index: Index) {
         callbackQueue.async { [weak self] in
             guard let self = self else {
                 return
             }
-            
+
             self.onLoadBefore(
                 index,
-                { [weak self] (result) in
+                { [weak self] result in
                     guard let self = self else {
                         return
                     }
-                    
+
                     self.requests.updateAsync({ (requests) -> ([LazyRequest<Element>?]) in
                         var updatedRequests = requests
                         updatedRequests.remove(at: 0)
-                        
+
                         if let result = result, !result.isEmpty {
                             updatedRequests.insert(contentsOf: result.map { LazyRequest.wrap(value: $0) }, at: 0)
                             updatedRequests.insert(nil, at: 0)
                         } else {
                             updatedRequests.append(LazyRequest.wrap(value: nil, error: nil))
                         }
-                        
+
                         return updatedRequests
                     })
                 },
-                { [weak self] (error) in
+                { [weak self] error in
                     self?.requests[index - 1] = LazyRequest.wrap(error: error)
                 }
             )
         }
     }
-    
+
     private func triggerLoadAfter(index: Index) {
         callbackQueue.async { [weak self] in
             guard let self = self else {
@@ -290,44 +287,44 @@ class LazyList<Element> {
 
             self.onLoadAfter(
                 index,
-                { [weak self] (result) in
+                { [weak self] result in
                     guard let self = self else {
                         return
                     }
-                    
+
                     self.requests.updateAsync({ (requests) -> ([LazyRequest<Element>?]) in
                         var updatedRequests = requests
                         updatedRequests.remove(at: updatedRequests.count - 1)
-                        
+
                         if let result = result, !result.isEmpty {
                             updatedRequests.insert(contentsOf: result.map { LazyRequest.wrap(value: $0) }, at: updatedRequests.count)
                             updatedRequests.append(nil)
                         } else {
                             updatedRequests.append(LazyRequest.wrap(value: nil, error: nil))
                         }
-                        
+
                         return updatedRequests
                     })
                 },
-                { [weak self] (error) in
+                { [weak self] error in
                     self?.requests[index + 1] = LazyRequest.wrap(error: error)
                 }
             )
         }
     }
-    
+
     private func triggerLoadItem(index: Index) {
         callbackQueue.async { [weak self] in
             guard let self = self else {
                 return
             }
-            
+
             self.onLoadItem(
                 index,
-                { [weak self] (result) in
+                { [weak self] result in
                     self?.requests[index] = LazyRequest.wrap(value: result)
                 },
-                { [weak self] (error) in
+                { [weak self] error in
                     self?.requests[index] = LazyRequest.wrap(error: error)
                 }
             )
--- Lazy/ViewModel.swift
+++ Lazy/ViewModel.swift
@@ -14,21 +14,21 @@ extension DefaultCellItem {
     struct Constants {
         static let errorColor = UIColor(hexString: "c62828")!
         static let errorEmoji = emojiList["no_entry"]!
-        
+
         static let loadingColor = UIColor(hexString: "9e9e9e")!
         static let loadingEmoji = emojiList["sleeping"]!
     }
-    
+
     static func from(result: LazyResult<ColorEmoji>?) -> DefaultCellItem {
         if let error = result?.error {
             let text = (error as? LoadingError)?.description ?? "unknown"
             return DefaultCellItem(color: Constants.errorColor, emoji: Constants.errorEmoji, text: text)
         }
-        
+
         if let item = result?.value {
             return DefaultCellItem(color: item.color, emoji: item.emoji, text: "\(item.index): \(item.emojiName)")
         }
-        
+
         return DefaultCellItem(color: Constants.loadingColor, emoji: Constants.loadingEmoji, text: "loading...")
     }
 }
@@ -39,33 +39,33 @@ class ViewModel {
         static let simulatedDatasetSize = 50
         static let simulatedDelay = 0.002
     }
-    
+
     let backend = Backend(simulatedDatasetSize: Constants.simulatedDatasetSize, simulatedDelay: Constants.simulatedDelay)
 
     private let currentItemsAccess = DispatchQueue(label: "CurrentItemsAccessQueue")
-    
-    var callback: (([DefaultCellItem]?, [DefaultCellItem]?, [Int]) -> ())? = nil
+
+    var callback: (([DefaultCellItem]?, [DefaultCellItem]?, [Int]) -> Void)?
 
     var currentItems = [LazyResult<ColorEmoji>?]()
     private lazy var cache = LazyList<ColorEmoji>(
-        onLoadBefore: { (index, onSuccess, onError) in
+        onLoadBefore: { index, onSuccess, onError in
             log.debug("fetching items BEFORE index: \(index)")
-            
+
             // randomize the size of the generated page - but don't return an empty list!
-            let pageSize: Int = Int.random(in: 0...Constants.maxPageSize)
-            self.backend.loadEmojis(skip: index - 1 - pageSize, top: pageSize, callback: { (result, error) in
+            let pageSize: Int = Int.random(in: 0 ... Constants.maxPageSize)
+            self.backend.loadEmojis(skip: index - 1 - pageSize, top: pageSize, callback: { result, error in
                 if let error = error {
                     onError(error)
                     return
                 }
-                
+
                 onSuccess(result)
             })
-    },
-        onLoadItem: { (index, onSuccess, onError) in
+        },
+        onLoadItem: { index, onSuccess, onError in
             log.debug("fetching item AT index: \(index)")
-            
-            self.backend.loadEmojis(skip: index, top: 1, callback: { (result, error) in
+
+            self.backend.loadEmojis(skip: index, top: 1, callback: { result, error in
                 if let error = error {
                     log.error("loading item AT index: \(index) failed with error: \(error)")
                     onError(error)
@@ -75,11 +75,11 @@ class ViewModel {
                 log.debug("loading item AT index: \(index) finished successfully")
                 onSuccess(result?.first)
             })
-    },
-        onLoadAfter: { (index, onSuccess, onError) in
+        },
+        onLoadAfter: { index, onSuccess, onError in
             log.debug("fetching items AFTER index: \(index)")
-            let pageSize = Int.random(in: 1...Constants.maxPageSize)
-            self.backend.loadEmojis(skip: index + 1, top: pageSize, callback: { (result, error) in
+            let pageSize = Int.random(in: 1 ... Constants.maxPageSize)
+            self.backend.loadEmojis(skip: index + 1, top: pageSize, callback: { result, error in
                 if let error = error {
                     log.error("loading item AFTER index: \(index) failed with error: \(error)")
                     onError(error)
@@ -89,19 +89,19 @@ class ViewModel {
                 log.debug("loading item AT index: \(index) finished successfully (pageSize: \(result?.count ?? 0), requestedSize: \(pageSize)")
                 onSuccess(result)
             })
-    },
+        },
         onChanged: { [weak self] in
             log.debug("ZEFIX - calling update()")
             self?.update()
-    })
-    
-    init() {
-    }
-    
+        }
+    )
+
+    init() {}
+
     func item(at index: Int) -> DefaultCellItem {
         return DefaultCellItem.from(result: cache[index])
     }
-    
+
     func prefetch(index: Int) {
         cache.prefetch(index: index)
     }
@@ -120,8 +120,8 @@ class ViewModel {
                 self.callback?(previousItems.map { DefaultCellItem.from(result: $0) },
                                self.currentItems.map { DefaultCellItem.from(result: $0) },
                                self.currentItems.enumerated()
-                                .filter { $0.element == nil }
-                                .map { $0.offset })
+                                   .filter { $0.element == nil }
+                                   .map { $0.offset })
             }
         }
     }
